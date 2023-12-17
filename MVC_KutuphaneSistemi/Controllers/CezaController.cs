2ee6f4f3f95dcbc1a268793e15c7bc6eb69735cc
Ali Can Dogan <76850300+doganalican46@users.noreply.github.com>
2023-12-17 17:28:30 +0300

    datatables added

---
+++ MVC_KutuphaneSistemi/Controllers/CezaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_KutuphaneSistemi.Controllers
+{
+    public class CezaController : Controller
+    {
+        // GET: Ceza
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
