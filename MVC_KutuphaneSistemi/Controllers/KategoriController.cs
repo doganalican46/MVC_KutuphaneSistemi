80fec00e16c4ead23512fa0eca8873a29b7d968e
Ali Can Dogan <76850300+doganalican46@users.noreply.github.com>
2023-12-17 14:09:23 +0300

    Add project files.

---
+++ MVC_KutuphaneSistemi/Controllers/KategoriController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_KutuphaneSistemi.Models.Entity;
+namespace MVC_KutuphaneSistemi.Controllers
+{
+    public class KategoriController : Controller
+    {
+        DBKutuphaneEntities db = new DBKutuphaneEntities();
+        // GET: Kategori
+        public ActionResult Index()
+        {
+            var degerler = db.Kategoriler.ToList();
+            return View(degerler);
+        }
+    }
+}
---
+++ MVC_KutuphaneSistemi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC_KutuphaneSistemi.MvcApplication" Language="C#" %>
