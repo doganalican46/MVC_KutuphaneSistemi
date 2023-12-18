5a52dc1af4a0dd153a017ad44bbdd22fa3c5ad91
Ali Can Dogan <76850300+doganalican46@users.noreply.github.com>
2023-12-18 12:41:46 +0300

    personel ve üye sayfaları tamamlandı

--- MVC_KutuphaneSistemi/Controllers/PersonelController.cs
+++ MVC_KutuphaneSistemi/Controllers/PersonelController.cs
@@ -3,15 +3,56 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using MVC_KutuphaneSistemi.Models.Entity;
 namespace MVC_KutuphaneSistemi.Controllers
 {
     public class PersonelController : Controller
     {
+        DBKutuphaneEntities db = new DBKutuphaneEntities();
         // GET: Personel
         public ActionResult Index()
         {
+            var personeller = db.Personeller.ToList();
+            return View(personeller);
+        }
+
+        [HttpGet]
+        public ActionResult YeniPersonel()
+        {
             return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult YeniPersonel(Personeller k)
+        {
+            db.Personeller.Add(k);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult PersonelSil(int id)
+        {
+            var Personel = db.Personeller.Find(id);
+            Personel.Durum = false;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult PersonelGetir(int id)
+        {
+            var Personel = db.Personeller.Find(id);
+            return View("PersonelGetir", Personel);
+        }
+
+        public ActionResult PersonelGuncelle(Personeller k)
+        {
+            var Personel = db.Personeller.Find(k.ID);
+            Personel.Ad = k.Ad;
+            Personel.Soyad = k.Soyad;
+            Personel.Durum = k.Durum;
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
--- MVC_KutuphaneSistemi/Controllers/UyeController.cs
+++ MVC_KutuphaneSistemi/Controllers/UyeController.cs
@@ -3,15 +3,63 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MVC_KutuphaneSistemi.Models.Entity;
 
 namespace MVC_KutuphaneSistemi.Controllers
 {
     public class UyeController : Controller
     {
+        DBKutuphaneEntities db = new DBKutuphaneEntities();
+
         // GET: Uyeler
         public ActionResult Index()
         {
+            var Uyeler = db.Uyeler.ToList();
+            return View(Uyeler);
+        }
+
+        [HttpGet]
+        public ActionResult YeniUye()
+        {
             return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult YeniUye(Uyeler k)
+        {
+            db.Uyeler.Add(k);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult UyeSil(int id)
+        {
+            var Uye = db.Uyeler.Find(id);
+            Uye.Durum = false;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult UyeGetir(int id)
+        {
+            var Uye = db.Uyeler.Find(id);
+            return View("UyeGetir", Uye);
+        }
+
+        public ActionResult UyeGuncelle(Uyeler k)
+        {
+            var Uye = db.Uyeler.Find(k.ID);
+            Uye.Ad = k.Ad;
+            Uye.Soyad = k.Soyad;
+            Uye.Mail = k.Mail;
+            Uye.Telefon= k.Telefon;
+            Uye.OkulAdi = k.OkulAdi;
+            Uye.KullaniciAdi = k.KullaniciAdi;
+            Uye.Fotograf= k.Fotograf;
+            Uye.Durum = k.Durum;
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
