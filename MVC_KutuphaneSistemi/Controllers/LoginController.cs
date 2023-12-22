05739bc6aebe52dfdc871e9d1e2f6cd030fc9737
Ali Can Dogan <76850300+doganalican46@users.noreply.github.com>
2023-12-22 22:07:22 +0300

    user interface and ogin register forms created

---
+++ MVC_KutuphaneSistemi/Controllers/LoginController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using MVC_KutuphaneSistemi.Models.Entity;
+namespace MVC_KutuphaneSistemi.Controllers
+{
+    public class LoginController : Controller
+    {
+        DBKutuphaneEntities db = new DBKutuphaneEntities();
+        // GET: Login
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Uyeler u)
+        {
+            var bilgiler = db.Uyeler.FirstOrDefault(x => x.Mail == u.Mail && x.Sifre == u.Sifre);
+            if (bilgiler != null)
+            {
+                FormsAuthentication.SetAuthCookie(bilgiler.Mail, false);
+                return RedirectToAction("Index","User");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Register(Uyeler u)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Register");
+            }
+            db.Uyeler.Add(u);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MVC_KutuphaneSistemi/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_KutuphaneSistemi.Models.Entity;
+namespace MVC_KutuphaneSistemi.Controllers
+{
+    public class UserController : Controller
+    {
+        DBKutuphaneEntities db = new DBKutuphaneEntities();
+        // GET: User
+        [Authorize]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
